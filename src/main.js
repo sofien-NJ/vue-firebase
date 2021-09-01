4d40bb997dba34484df9aa8024acf2a1b0f421f3
sofien njeimi <7sofien@gmail.com>
2021-09-01 20:51:23 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
+import Vue2Editor from "vue2-editor";
+import axios from "axios";
+import firebase from "firebase/app";
 
-createApp(App).use(router).mount("#app");
+
+Vue.use(Vue2Editor);
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
